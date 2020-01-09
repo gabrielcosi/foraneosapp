956f6970a56a67cdbbd46cb4cc8548aa1c1c3008
nickmeza <nickmeza@upeu.edu.pe>
2020-01-09 15:32:32 -0500

    hola

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:foraneos/vistas/mapa.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,14 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home: MyHomePage()
     );
   }
 }
---
+++ lib/vistas/mapa.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var scaffold = Scaffold(
+        endDrawer: Drawer(
+          child: Container(
+            width: 10.00,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(80.0),
+                  child: Text(
+                    "data",
+                    style: TextStyle(fontSize: 50),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        appBar: AppBar(
+          title: Text("Foraneos"),
+          actions: <Widget>[
+            InkWell(
+              child: Icon(Icons.delete_forever),
+              onTap: () {
+                print("hello");
+              },
+            ),
+            Icon(Icons.search),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Icon(Icons.dehaze),
+            ),
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                "data",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            Container(
+              height: 100.0,
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (contex, position) {
+                  return Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      width: 200.0,
+                      height: 150.0,
+                      color: Colors.blue,
+                    ),
+                  );
+                },
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+            DefaultTabController(
+              length: 3,
+              child: Expanded(
+                child: Column(
+                  children: <Widget>[
+                    TabBar(
+                      unselectedLabelColor: Colors.grey,
+                      indicatorColor: Colors.blue,
+                      tabs: [
+                        Tab(
+                          text: "Cursos",
+                        ),
+                        Tab(
+                          text: "Works",
+                        ),
+                        Tab(
+                          text: "Especialidades",
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                        child: TabBarView(children: [
+                      GridView.builder(
+                        itemCount: 10,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2),
+                        itemBuilder: (context, position) {
+                          return Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Container(
+                              width: 200.0,
+                              height: 150.0,
+                              child: Image.network(
+                                  "https://www.crearlogogratisonline.com/images/crearlogogratis_1024x1024_01.png"),
+                            ),
+                          );
+                        },
+                      ),
+                      Container(
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        color: Colors.brown,
+                      ),
+                    ]))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
+    return scaffold;
+  }
+}
